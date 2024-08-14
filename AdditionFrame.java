24247f5dedf722c7e1969399834eaf4438b11d80
ujjwal dethe <ujwaldethe518@gmail.com>
2024-08-14 09:34:04 +0530

    Package Changed

---
+++ AdditionFrame.java
@@ -0,0 +1,57 @@
+package AdditionUsingGUI;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+
+public class AdditionFrame extends JFrame{
+    private JPanel panel;
+    private JLabel L1;
+    private JLabel L2;
+    private JLabel L3;
+    private JTextField T1;
+    private JTextField T2;
+    private JButton B1;
+
+    public void initComponents()
+    {
+        panel.setLayout(null);
+        L1.setBounds(50,50,100,20);
+        T1.setBounds(170,50,100,20);
+        L2.setBounds(50,100,100,20);
+        T2.setBounds(170,100,100,20);
+        B1.setBounds(130,150,100,40);
+        L3.setBounds(150,230,200,20);
+        panel.add(L1);
+        panel.add(T1);
+        panel.add(L2);
+        panel.add(T2);
+        panel.add(B1);
+        panel.add(L3);
+        B1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try{
+                int a = Integer.parseInt(T1.getText());
+                int b = Integer.parseInt(T2.getText());
+                int c =  a + b ;
+                L3.setText("Addition : "+c);
+                }
+                catch(Exception c)
+                {
+                    L3.setText("Number Tak Lawdya");
+                }
+            }
+        });
+    }
+    public AdditionFrame()
+    {
+       initComponents();
+       setContentPane(panel);
+       setSize(400,400);
+       setVisible(true);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
---
+++ Hello.java
@@ -0,0 +1,42 @@
+package world;
+import Assignments.AccessModifiers;
+import Assignments.*;
+import java.time.LocalDateTime;
+
+import java.time.chrono.ChronoLocalDateTime;
+
+import static java.lang.Math.sqrt;
+
+public class Hello {
+    public static void main(String args[])
+    {
+
+//        AdditionFrame frame = new AdditionFrame();
+//        int n = 4 ;
+//        boolean flag = false;
+//        for(int i =0 ; i < sqrt(n) ; i++)
+//        {
+//            if(n%2==0)
+//            {
+//                flag=true;
+//                break;
+//            }
+//        }
+//        if(flag==true)
+//        {
+//            System.out.println("Not Prime");
+//        }
+//        else
+//        {
+//            System.out.println("Prime");
+//        }
+//        Assignments.AccessModifiers obj = new Assignments.AccessModifiers();
+//        obj.driver();
+//
+//        LocalDateTime T = LocalDateTime.now();
+//        System.out.println("\nTime : "+T);
+      
+
+    }
+
+}
